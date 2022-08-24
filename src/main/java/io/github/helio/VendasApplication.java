2129596396c8bc9fb7932b054acdf3ccf9724208
Helio-anacronista <heliojobs25@gmail.com>
2022-08-24 16:05:18 -0300

    Basic crud cliente jdbc spring

--- src/main/java/io/github/helio/VendasApplication.java
+++ src/main/java/io/github/helio/VendasApplication.java
@@ -20,17 +20,38 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes) {
         return args -> {
-            clientes.salvar(new Cliente("Primerio-Cliente"));
-            clientes.salvar(new Cliente("Segundo-Cliente"));
+            System.out.println("Salvando clientes");
+            clientes.salvar(new Cliente("Cliente-Helio"));
+            clientes.salvar(new Cliente("Cliente-Thiago"));
 
             java.util.List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
 
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.atualizar(c);
+            });
+
+            System.out.println("Buscando clientees");
+            clientes.buscarPorNome("Heli").forEach(System.out::println);
+
+
+//            System.out.println("Delentado clientes");
+//            clientes.obterTodos().forEach(c -> {
+//                clientes.deletar(c);
+//            });
+            todosClientes = clientes.obterTodos();
+            if (todosClientes.isEmpty()) {
+                System.out.println("Nenhum Cliente encontrado");
+            } else {
+                todosClientes.forEach(System.out::println);
+            }
+
+
+
         };
     }
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
--- src/main/java/io/github/helio/domain/repositorio/Clientes.java
+++ src/main/java/io/github/helio/domain/repositorio/Clientes.java
@@ -13,26 +13,56 @@ import java.util.List;
 @Repository
 public class Clientes {
 
+    //Script do jpa
     private static String INSERT = "insert into cliente (nome) values (?) ";
     private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
+    private static String UPDATE = "update cliente set nome = ? where id = ? ";
+    private static String DELETE = "delete from cliente where id = ? ";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
+    //Salvar os cliente e injetar
     public Cliente salvar(Cliente cliente)  {
         jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
         return cliente;
     }
 
+    public Cliente  atualizar(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[] {cliente.getNome(), cliente.getId()});
+        return cliente;
+    }
+
+    //Delete por id
+    public void deletar(Cliente cliente) {
+        deletar(cliente.getId());
+    }
+    public void deletar(Integer id) {
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    //Buscar por nome
+    public List<Cliente> buscarPorNome(String nome) {
+        return jdbcTemplate.query(SELECT_ALL.concat(" where nome like ? "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
+    }
+
+
+
+    //Imprimir na classe Cliente os cliente obtidos que foram salvado no bando de dados
     public List<Cliente> obterTodos()  {
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private static RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
                 Integer id = resultSet.getInt("id");
                 String nome = resultSet.getString("nome");
                 return new Cliente(id, nome);
             }
-        });
+        };
     }
 
 }
